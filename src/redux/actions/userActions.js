c6293380623f69dcd7aab4a08dfa9cdf53fe240b
Sinan Işık <sinan.isik@ug.bilkent.edu.tr>
2020-05-29 21:20:08 +0300

    add profile section

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -4,6 +4,7 @@ import {
   CLEAR_ERRORS,
   LOADING_UI,
   SET_UNAUTHENTICATED,
+  LOADING_USER,
 } from "../types";
 import axios from "axios";
 
@@ -60,6 +61,7 @@ export const logoutUser = () => (dispatch) => {
 };
 
 export const getUserData = () => (dispatch) => {
+  dispatch({ type: LOADING_USER });
   axios
     .get("/user")
     .then((res) => {
